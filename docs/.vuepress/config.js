74f97ca8bca50d4079e3c7b553e45e5843971060
Vikas <kedia.vikas@gmail.com>
2020-06-27 06:31:26 -0700

    component docgen is working

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,15 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around'
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    sidebar: ['/'],
+  },
+  plugins: [
+    [
+      'component-docgen',
+      {
+        rootDir: '/Users/vk-tech/gt/sc-prog-repos/ptclient/components/',
+      },
+    ],
+  ],
 }
